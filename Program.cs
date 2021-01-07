1cafd13fd03d1bc432876155a69e25bf96417203
peshraw hasan <66382977+pesha19@users.noreply.github.com>
2021-01-07 19:32:36 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Business_Application_Development
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - 2001;
+            Console.WriteLine("You are "+age+" years old");
+        }
+    }
+}
